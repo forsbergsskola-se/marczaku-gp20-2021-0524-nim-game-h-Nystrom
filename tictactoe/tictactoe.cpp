e751b59b69606250c433058fd7dd3d24b677af22
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-05-25 12:20:08 +0200

    Adds tile-map

--- tictactoe/tictactoe.cpp
+++ tictactoe/tictactoe.cpp
@@ -1,6 +1,28 @@
+using namespace std;
+#include <iostream>
+#include <string>
+#include <limits>
 
-int main(int argc, char* argv[])
-{
+
+void UpdateUi(string tileMap[]){
+    const int rowSize = 3;
     
-    return 0;
+    for (int i = 0; i <=  sizeof(tileMap);i++){
+        cout << " | " + tileMap[i] << flush;
+        const int tempNumber = i + 1;
+        if(tempNumber % rowSize == 0){
+            cout << " | " << endl;
+            cout << "---------------" << endl;
+        }
+            
+    }
+}
+bool IsTileEmpty(string tile){
+    return tile == "";
+}
+
+int main()
+{
+    string tiles[9];
+    UpdateUi(tiles);
 }
