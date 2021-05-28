9eab3b312d4021691c7089eb26345775a529636b
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-05-28 16:52:31 +0200

    Update BinaryConverter.cpp
    +Can now differentiate between a 0 and char input

--- BinaryConverter/BinaryConverter.cpp
+++ BinaryConverter/BinaryConverter.cpp
@@ -5,20 +5,6 @@ using namespace std;
 #include <string>
 #include <limits>
 
-    uint32_t GetUserInputValue(){
-        const uint32_t maxValue = numeric_limits<uint32_t>::max(); 
-        while (true){
-            uint32_t userInputValue;
-            cout << "Input a positive integer: " << endl;
-            cin >> noskipws >> userInputValue;
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(),'\n');
-            if(userInputValue && userInputValue <= maxValue)
-                return userInputValue;
-            
-            cout << "Input needs to be a positive number!"<< endl;
-        }
-    }
     vector<bool> ConvertUnsignedIntToBinary(uint32_t inputValue){
         auto boolArray = new vector<bool>;
         
@@ -61,15 +47,37 @@ using namespace std;
             getline(cin, inputValue);
             return inputValue == valueCheck;
     }
+
+    void ResetCin(){
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+    }
+    uint32_t GetUserInputValue(){
+        const uint32_t maxValue = numeric_limits<uint32_t>::max(); 
+        while (true){
+            uint64_t userInputValue;
+            cout << "Input a positive integer: " << endl;
+            cin >> userInputValue;
+            if(cin.good()){
+                if(userInputValue >= 0 && userInputValue <= maxValue){
+                    ResetCin();
+                    return userInputValue;
+                }
+                    
+            }
+            ResetCin();
+            cout << "Input needs to be a positive number between: 0 and " + to_string(maxValue) + "!"<< endl;
+        }
+    }
     int main(){
         while (true){
             const uint32_t userInputValue = GetUserInputValue();
             const vector<bool> boolArray = ConvertUnsignedIntToBinary(userInputValue);
             PrintBinaryNumber(boolArray);
             BitwiseConvertAndPrint(userInputValue);
-            if(BooleanInputCheck("X", "Press x to quit!"))
+            if(BooleanInputCheck("x", "Press x to quit!"))
                 break;
         }
-        cout << "Shutting down" << endl;
+        cout << "Shutting down!" << endl;
         return 0;
     }
